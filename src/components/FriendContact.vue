d95a0d764102be8e397242d321e3f0cb4ed06ed0
Derrick Strong <derrickmstrong@gmail.com>
2021-02-04 14:51:26 -0600

    first commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click='toggleDetails'>{{ isDetailsVisible ? 'Hide' : 'Show'}} Details</button>
+    <ul v-if='isDetailsVisible'>
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDetailsVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: "0123 45678 90",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      return (this.isDetailsVisible = !this.isDetailsVisible);
+    },
+  },
+};
+</script>
